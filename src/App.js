3f1c20066323a24a67701312fc8b970457e71de5
Mher <mherqi@gmail.com>
2020-12-28 00:39:36 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Input from './Components/input';
-import Stylemode from './Components/styleMode';
+
 
 
 
@@ -8,7 +8,6 @@ import Stylemode from './Components/styleMode';
 const App = () => {
   return (
     <div>
-      <Stylemode/>
       <Input/>
     </div>
   );
